a20b3d589c7a05466850014e5be88cfccf4feda7
meisolated <isolatedbot@gmail.com>
2023-01-15 11:21:41 +0530

    first commit

---
+++ lib/trueData/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios"
+import qs from "qs"
+import logger from "../../logger"
+
+class TrueData {
+    username: string
+    password: string
+    accessToken: string
+    baseUrl: string = "https://auth.truedata.in"
+    constructor(username: string, password: string) {
+        this.username = username
+        this.password = password
+    }
+
+    async initializeAccessToken() {
+        // get accessToken
+        const data = qs.stringify({
+            username: this.username,
+            password: this.password,
+            grant_type: "password",
+        })
+        const config = { method: "post", url: `${this.baseUrl}/token`, headers: { "Content-Type": "application/x-www-form-urlencoded" }, data: data }
+        axios(config).then((response: any) => {
+            const data = response.data
+            this.accessToken = data.access_token
+            return true
+        }).catch((err: any) => {
+            logger.error(err)
+            return false
+        })
+    }
+}
+export default TrueData
